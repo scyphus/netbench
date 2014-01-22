072c4d8a804b570ceca3aff754cc1b3bb8ff59e1
Hirochika Asai <asai@scyphus.co.jp>
2014-01-22 10:02:25 +0900

    Added initial files.

---
+++ include/netbench.h
@@ -0,0 +1,28 @@
+/*_
+ * Copyright 2014 Scyphus Solutions Co. Ltd.  All rights reserved.
+ *
+ * Authors:
+ *      Hirochika Asai  <asai@scyphus.co.jp>
+ */
+
+#ifndef _NETBENCH_H
+#define _NETBENCH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _NETBENCH_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
---
+++ libnb/netbench_private.h
@@ -0,0 +1,20 @@
+/*_
+ * Copyright 2014 Scyphus Solutions Co. Ltd.  All rights reserved.
+ *
+ * Authors:
+ *      Hirochika Asai  <asai@scyphus.co.jp>
+ */
+
+#ifndef _NETBENCH_PRIVATE_H
+#define _NETBENCH_PRIVATE_H
+
+#endif /* _NETBENCH_PRIVATE_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
