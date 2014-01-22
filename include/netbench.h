184202a776604ed95bc713b16e6ff3133a990105
Hirochika Asai <asai@scyphus.co.jp>
2014-01-22 13:37:23 +0900

    Implementing HTTP (GET/POST) throughput measurement functions.

--- include/netbench.h
+++ include/netbench.h
@@ -67,6 +67,62 @@ struct _traceroute {
     int cancel;
 };
 
+/*
+ * HTTP (GET)
+ */
+typedef struct _http_get_result_item {
+    double tm;
+    off_t tx;
+    off_t rx;
+    off_t brx;
+} nb_http_get_result_item_t;
+typedef struct _http_get_result {
+    size_t cnt;
+    size_t cntres;
+    nb_http_get_result_item_t *items;
+    size_t hlen;
+    size_t clen;
+} nb_http_get_result_t;
+typedef struct _http_get nb_http_get_t;
+typedef void (*nb_http_get_cb_f)(nb_http_get_t *, size_t, size_t, double,
+                                 size_t, double);
+struct _http_get {
+    nb_http_get_cb_f cb;
+    void *user;
+    char *mid;
+    nb_http_get_result_t *last_result;
+    int cancel;
+};
+
+/*
+ * HTTP (POST)
+ */
+typedef struct _http_post_result_item {
+    double tm;
+    size_t tx;
+    size_t rx;
+    size_t btx;                 /* Buffered TX */
+} nb_http_post_result_item_t;
+typedef struct _http_post_result {
+    size_t cnt;
+    size_t cntres;
+    nb_http_post_result_item_t *items;
+    size_t hlen;
+    size_t clen;
+} nb_http_post_result_t;
+typedef struct _http_post nb_http_post_t;
+typedef void (*nb_http_post_cb_f)(nb_http_post_t *, size_t, size_t, double,
+                                  size_t, double);
+struct _http_post {
+    nb_http_post_cb_f cb;
+    char *mid;
+    void *user;
+    nb_http_post_result_t *last_result;
+    int cancel;
+};
+
+
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -88,6 +144,18 @@ extern "C" {
     nb_traceroute_set_callback(nb_traceroute_t *, nb_traceroute_cb_f, void *);
     int nb_traceroute_exec(nb_traceroute_t *, const char *, int, int, double);
     void nb_traceroute_delete(nb_traceroute_t *);
+
+    /* HTTP (GET) */
+    nb_http_get_t * nb_http_get_new(const char *);
+    int nb_http_get_set_callback(nb_http_get_t *, nb_http_get_cb_f, void *);
+    int nb_http_get_exec(nb_http_get_t *, const char *, double);
+    void nb_http_get_delete(nb_http_get_t *);
+
+    /* HTTP (POST) */
+    nb_http_post_t * nb_http_post_new(const char *);
+    int nb_http_post_set_callback(nb_http_post_t *, nb_http_post_cb_f, void *);
+    int nb_http_post_exec(nb_http_post_t *, const char *, size_t, double);
+    void nb_http_post_delete(nb_http_post_t *);
 
 
 #ifdef __cplusplus
